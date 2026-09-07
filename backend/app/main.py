cae8b2e25507a588009af3f24d9542bb9982a112
Khanhpro098788 <vovankhanh937@gmail.com>
2026-09-07 09:09:24 +0700

    fix: update CORS to allow deployed frontend url

--- backend/app/main.py
+++ backend/app/main.py
@@ -44,7 +44,11 @@ app = FastAPI(
 # Setup CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],  # Frontend URL
+    allow_origins=[
+        "http://localhost:5173", 
+        "http://127.0.0.1:5173",
+        "https://frontend-service-hdn33p3fcq-as.a.run.app" # Deployed Frontend URL
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
